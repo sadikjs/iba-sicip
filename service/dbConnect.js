01f2d17e602c9dfc2662671f23a3eb5fa3be2407
Sadik <sadik@Sadiks-MacBook-Pro.local>
2025-02-06 17:08:14 +0600

    connection update commit 14

--- service/dbConnect.js
+++ service/dbConnect.js
@@ -10,12 +10,7 @@ async function dbConnect() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
