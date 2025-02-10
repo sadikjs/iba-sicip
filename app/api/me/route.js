2f3d537bac4296f637ecca9f76de46f6ba165b7f
Sadik <sadik@Sadiks-MacBook-Pro.local>
2025-02-10 11:11:29 +0600

    commit rule breack to new again commit 15

--- app/api/me/route.js
+++ app/api/me/route.js
@@ -4,21 +4,24 @@ import dbConnect from "@/service/dbConnect";
 import { NextResponse } from "next/server";
 
 export const GET = async (req, res) => {
-    const session = await auth();
-    if (!session?.user) {
-        return new NextResponse(`You are not authenticated!`, {
-            status: 401,
-        });
-    }
-    await dbConnect();
-    try {
-        const user = await getUserByEmail(session?.user?.email);
-        return new NextResponse(JSON.stringify(user), {
-            status: 200,
-        });
-    } catch (err) {
-        return NextResponse.json({meg:err.message},{
-            status: 500,
-        });
-    }
-}
+  const session = await auth();
+  if (!session?.user) {
+    return new NextResponse(`You are not authenticated!`, {
+      status: 401,
+    });
+  }
+  await dbConnect();
+  try {
+    const user = await getUserByEmail(session?.user?.email);
+    return new NextResponse(JSON.stringify(user), {
+      status: 200,
+    });
+  } catch (err) {
+    return NextResponse.json(
+      { meg: err.message },
+      {
+        status: 500,
+      }
+    );
+  }
+};
